b898d3d6f0557769fd5fb0344229564600a907a7
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-09-05 13:58:37 +1200

    Added files for depthFirst path finding algorithm

---
+++ depthFirst/codegen/lib/depthFirstFunction/depthFirstFunction_initialize.h
@@ -0,0 +1,26 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * depthFirstFunction_initialize.h
+ *
+ * Code generation for function 'depthFirstFunction_initialize'
+ *
+ */
+
+#ifndef DEPTHFIRSTFUNCTION_INITIALIZE_H
+#define DEPTHFIRSTFUNCTION_INITIALIZE_H
+
+/* Include files */
+#include <stddef.h>
+#include <stdlib.h>
+#include "rtwtypes.h"
+#include "depthFirstFunction_types.h"
+
+/* Function Declarations */
+extern void depthFirstFunction_initialize();
+
+#endif
+
+/* End of code generation (depthFirstFunction_initialize.h) */
