eaf8a0c11fc124461539c68fc5b763fb4fc77329
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2021-09-09 10:50:47 +1200

    Add files via upload

---
+++ DepthFirst/Matlab/runDepthFirst.m
@@ -0,0 +1,11 @@
+maze = [1 1 0 1; 1 0 0 0; 1 1 0 1;1 0 0 1];
+
+startingRow = 1;
+startingCol = 3;
+endRow = 4;
+endCol = 2;
+
+disp("Original maze to navigate")
+disp(maze)
+
+depthFirstFunction(maze,startingRow, startingCol, endRow, endCol);
