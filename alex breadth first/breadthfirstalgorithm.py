0ad394dc1e3c1b4eba1e34f2c6281e1d723065d4
acol547 <68828948+acol547@users.noreply.github.com>
2021-09-25 15:57:04 +1200

    Modifying algorithm to not move backwards on final move in list
    I.e final two moves can't be move right and then move left

--- alex breadth first/breadthfirstalgorithm.py
+++ alex breadth first/breadthfirstalgorithm.py
@@ -4,7 +4,7 @@ import numpy as np
 maze = []
 with open('map.txt', 'r') as f:
     
-    for i in range (15):
+    for i in range (15):                                     
         lines = f.readline()
         lines = list(lines)
         lines.pop()
@@ -49,23 +49,39 @@ def valid(maze, moves, startPosX, startPosY):
 
     i = startPosX
     j = startPosY
-    for move in moves:
-        if move == "L":
-            i -= 1
+    latestMove = ""
+    for move in (moves):
+        if (move == "L"):
+            if (latestMove == "R"):
+                return False
+            else:
+                i-= 1
+                latestMove = "L"
+        elif (move == "R"):
+            if (latestMove == "L"):
+                return False
+            else:
+                i += 1
+                latestMove = "R"
+        elif (move == "U"):
+            if (latestMove == "D"):
+                return False
+            else:
+                j -= 1
+                latestMove = "U"
 
-        elif move == "R":
-            i += 1
+        elif (move == "D"):
+            if (latestMove == "U"):
+                return False
+            else: 
+                j += 1
+                latestMove = "D"
 
-        elif move == "U":
-            j -= 1
+    if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):                       # If co-ordinate is outside the maze, return false.
+        return False
+    elif (maze[j][i] == "1"):                                                   # If co-ordinate is a wall, return false
+        return False
 
-        elif move == "D":
-            j += 1
-
-        if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):                       # If co-ordinate is outside the maze, return false.
-            return False
-        elif (maze[j][i] == "1"):                                                   # If co-ordinate is a wall, return false
-            return False
     return True
 
 def findEnd(maze, moves, startPosX, startPosY, endPosX, endPosY):
@@ -98,13 +114,20 @@ nums.put("")
 add = ""
 startPosX = 1
 startPosY = 1
-endPosX = 2
-endPosY = 5
+endPosX = 1
+endPosY = 13
 
-while not findEnd(maze, add, startPosX, startPosY, endPosX, endPosY): 
-    add = nums.get()
-    #print(add)
-    for j in ["L", "R", "U", "D"]:
-        put = add + j
-        if valid(maze, put, startPosX, startPosY):
-            nums.put(put)
+if ((maze[startPosY][startPosX]=="1")|(maze[endPosY][endPosX]=="1")): 
+    print ("\nError: Invalid Start and/or End position\n")
+else:
+    while not findEnd(maze, add, startPosX, startPosY, endPosX, endPosY): 
+        add = nums.get()
+        for j in ["L", "R", "U", "D"]:
+            put = add + j
+            if valid(maze, put, startPosX, startPosY):
+                nums.put(put)
+        if nums.empty():
+            print("\nNo path found\n")
+            break
+
+    
