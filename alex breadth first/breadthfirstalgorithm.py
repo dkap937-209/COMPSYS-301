493e02c6e2da2ed7fe76e9400f468f77bcf5ba06
acol547 <68828948+acol547@users.noreply.github.com>
2021-09-26 13:26:13 +1300

    Adding comments to Breadth First code

--- alex breadth first/breadthfirstalgorithm.py
+++ alex breadth first/breadthfirstalgorithm.py
@@ -1,19 +1,21 @@
 import queue
 import numpy as np
 
+
+# Following section of code produces the maze as a 2-D list by reading from the map.txt file
 maze = []
 with open('map.txt', 'r') as f:
-    
-    for i in range (15):                                     #Change back to 15
+    # For loop to go through all lines of the map.txt file, remove the '\n' from each line, 
+    # then append onto our maze variable.
+    for i in range (15):                                     
         lines = f.readline()
         lines = list(lines)
         lines.pop()
         maze.append(lines)
     f.close()
 
-
+# Function to print the max to terminal once shortest path has been found.
 def printMaze(maze, startPosX, startPosY, endPosX, endPosY, path=""):
-
     i = startPosX
     j = startPosY
     pos = set()
@@ -69,18 +71,19 @@ def valid(maze, moves, startPosX, startPosY):
             j += 1
             latestMove = "D"
 
-    if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):                       # If co-ordinate is outside the maze, return false.
+    if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):       # If co-ordinate is outside the maze, return false.
         return False
-    elif (maze[j][i] == "1"):                                                   # If co-ordinate is a wall, return false
+    elif (maze[j][i] == "1"):                                   # If co-ordinate is a wall, return false
         return False
-    elif (maze[j][i] == "@"):
+    elif (maze[j][i] == "@"):                                   # IF co-ordinate has already been visited, return false.
         return False
 
-    maze[j][i] = "@"
+    maze[j][i] = "@"                                            #This is only reached when a valid path has been taken, 
+                                                                # mark it with unique symbol.
     return True
 
+# Function to evaluate if our path reaches the target position.
 def findEnd(maze, moves, startPosX, startPosY, endPosX, endPosY):
-
     i = startPosX
     j = startPosY
     for move in moves:
@@ -107,12 +110,14 @@ def findEnd(maze, moves, startPosX, startPosY, endPosX, endPosY):
 nums = queue.Queue()
 nums.put("")
 add = ""
+# Declare and define starting X and Y co-ordinate of Robot. (NOTE: Axis values start at 0)
 startPosX = 1
 startPosY = 1
+# Declare and define X an Y co-ordinate of target. (NOTE: Axis values start at 0)
 endPosX = 1
 endPosY = 13
 
-if ((maze[startPosY][startPosX]=="1")|(maze[endPosY][endPosX]=="1")): 
+if ((maze[startPosY][startPosX]=="1")|(maze[endPosY][endPosX]=="1")):       # If statement to check if given start and end position are valid
     print ("\nError: Invalid Start and/or End position\n")
 else:
     while not findEnd(maze, add, startPosX, startPosY, endPosX, endPosY): 
@@ -121,6 +126,6 @@ else:
             put = add + j
             if valid(maze, put, startPosX, startPosY):
                 nums.put(put)
-        if nums.empty():
+        if nums.empty():                                                    # If queue is empty, that means all paths have been evaluated and no path to target was found.
             print("\nNo path found\n")
             break
